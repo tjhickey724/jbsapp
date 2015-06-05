39c8e1984503351e82736f8731029fa018f8d6a0
zhjch05 <zjc1996@brandeis.edu>
2015-06-05 14:54:24 -0400

    sort

--- client/chat.js
+++ client/chat.js
@@ -1,5 +1,29 @@
 
 
 Template.chats.helpers({
-   chatData:function(){ return Chats.find();}
+   chatData:function(){ return Chats.find({},{sort:{createdAt: -1},limit:10});}
 });
+
+
+Template.chatinput.events({
+    
+    "submit #chatinputform": function(event){
+		event.preventDefault();
+
+		var msg = event.target.message.value;
+		console.log(JSON.stringify(this));
+   
+		Chats.insert({userName:Meteor.user().profile.userName,message:msg,createdAt:new Date()});
+
+		Router.go('/chat');
+		
+	},
+    
+    "submit #clearinput": function(event){
+		event.preventDefault();
+		Chats.remove({});
+
+		Router.go('/chat');
+		
+	}
+})
