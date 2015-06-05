f258ef8fa6461037bf21b1480d8e7aacddecab51
Jessica Huynh <jhuynh37@brandeis.edu>
2015-06-05 14:31:59 -0400

    working chatroom

--- client/chat.js
+++ client/chat.js
@@ -1,3 +1,19 @@
 Template.chats.helpers({
 	chatData: function() {return Chats.find();}
 });
+
+Template.chat.events({
+	"submit #chatform": function(event){
+		event.preventDefault();
+
+		var chatbox = event.target.chatbox.value;
+
+		Chats.insert({
+			username:Meteor.user().profile.userName,
+			createdAt:new Date(),
+			message:chatbox
+		});
+
+		document.getElementsByName('chatbox').value = '';
+	}
+})
