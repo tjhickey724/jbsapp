f65854249854007ca9419737af54b3fba73c25ba
kianajane <kkhozein@brandeis.edu>
2015-06-12 09:38:35 -0400

    basic design and functionality

--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,10 @@
 Meteor.publish("theProfiles",function(){return Profiles.find();});
-Meteor.publish("theIPs",function(){return IPAddresses.find();});
-Meteor.publish("theColors",function(){return Colors.find();});
+Meteor.publish("theAlbums",function(){return Albums.find();});
 
 
 Meteor.publish("userData", function () {
   if (this.userId) {
-	  return Meteor.users.find({}); //, //{_id: this.userId},
-                             //{fields: {'profile': 1, 'things': 1}});
+	  return Meteor.users.find({}); 
   } else {
     this.ready();
   }
--- server/startup.js
+++ server/startup.js
@@ -1,11 +1,1 @@
-Meteor.startup(function(){
-	if (Colors.find({}).count()==0){
-		// create some initial data for the collection...
-		Colors.insert({theColor:"red", hex:"ff0000"});
-
-		Colors.insert({theColor:"green", hex:"ff0000"});
-		Colors.insert({theColor:"blue", hex:"ff0000"});
-		Colors.insert({theColor:"aqua", hex:"ff0000"});
-		Colors.insert({theColor:"yellow", hex:"ff0000"});
-	}
-});
+Meteor.startup(function(){});
